f260428e01140e18569577db25c1b29d5bf389ae
Paweł Dobrowolski <pawwel3@gmail.com>
2020-04-19 10:11:32 +0200

    Updates

---
+++ precache-manifest.9b4cf01385d9995cac34b838f2878146.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "900a7c48ae46d5db56a07bbd9aa9f07f",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "b9c441566243fc735134",
+    "url": "/weather-app/static/css/main.28bceab0.chunk.css"
+  },
+  {
+    "revision": "43031bd8d672c9ddfb62",
+    "url": "/weather-app/static/js/2.713e8ce9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.713e8ce9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9c441566243fc735134",
+    "url": "/weather-app/static/js/main.7d7bf27a.chunk.js"
+  },
+  {
+    "revision": "8ad2e2c3d0b0209b3902",
+    "url": "/weather-app/static/js/runtime-main.4479c0a4.js"
+  },
+  {
+    "revision": "ab9718a25a4fb7ea0edad04e808d003e",
+    "url": "/weather-app/static/media/spring.ab9718a2.jpg"
+  }
+]);
