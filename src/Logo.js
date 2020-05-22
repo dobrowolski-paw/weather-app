82189f7b7ef2e473b1339b2eab770b3eefb304df
dobrowolski-paw <65705015+dobrowolski-paw@users.noreply.github.com>
2020-05-22 07:24:40 +0200

    Address change

--- src/Logo.js
+++ src/Logo.js
@@ -2,7 +2,7 @@ import React from "react";
 import sun from "./images/sun.png";
 const Logo = () => {
   return (
-    <a href="https://pawwel3.github.io/weather-app/">
+    <a href="https://dobrowolski-paw.github.io/weather-app/">
       <img src={sun} alt="słońce" style={{ float: "left" }}></img>
       <p style={{ color: "gray", float: "left" }}>miecio</p>
       <p style={{ color: "rgb(241, 168, 10", float: "left" }}>pogoda</p>
